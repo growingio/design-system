fbd23bab671393b653a2e408ccec531f4090fdee
Pan Jie <fenqi@startdt.com>
2025-03-05 10:31:40 +0800

    chore: export avatar

---
+++ src/avatar/index.ts
@@ -0,0 +1,4 @@
+import Avatar from "@arco-design/web-react/es/Avatar";
+
+export type { AvatarProps } from "@arco-design/web-react/es/Avatar";
+export default Avatar;
---
+++ src/avatar/style/index.ts
@@ -0,0 +1,1 @@
+import "@arco-design/web-react/es/Avatar/style/index.css";
