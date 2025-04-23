171a3210863ec1ef22c8f3d5c16aa836d559e73e
Pan Jie <fenqi@startdt.com>
2025-04-23 11:56:57 +0800

    docs: update zh-CN locale

--- locales/formatter.js
+++ locales/formatter.js
@@ -1,7 +1,7 @@
-exports.format = function (messages) {
+export function format(messages) {
   const results = {};
   for (const [id, message] of Object.entries(messages)) {
     results[id] = message.defaultMessage;
   }
   return results;
-};
+}
